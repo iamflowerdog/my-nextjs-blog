7c84de5666d7930e14539b3394e19bbd03b34fe7
yang.yang <yang.yang@polyvirtual.com>
2023-04-28 16:26:57 +0800

    feat: 搬运文章

--- src/lib/getAllArticles.js
+++ src/lib/getAllArticles.js
@@ -13,7 +13,7 @@ async function importArticle(articleFilename) {
 }
 
 export async function getAllArticles() {
-  let articleFilenames = await glob(['*.mdx', '*/index.mdx'], {
+  let articleFilenames = await glob(['*.mdx', '*/index.mdx', '*/*.mdx'], {
     cwd: path.join(process.cwd(), 'src/pages/articles'),
   })
 
