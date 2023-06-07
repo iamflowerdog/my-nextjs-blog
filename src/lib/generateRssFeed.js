a07aef4f75fc024e64f80660a2975b36dab34336
yang.yang <yangyonghai9125@hotmail.com>
2023-06-07 11:32:08 +0800

    feat: add my wife

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -8,8 +8,8 @@ export async function generateRssFeed() {
   let articles = await getAllArticles()
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Yang',
+    email: 'yangyonghai9125@hotmail.com',
   }
 
   let feed = new Feed({
